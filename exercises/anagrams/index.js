5d3e1662bc47c23f3961fec6603f4e99b16bb77d
kalunge <muthomititus+github@gmail.com>
2022-01-25 15:01:39 +0100

    Add Solution for Anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,39 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  // let charMapA = {};
+  // let charMapB = {};
+
+  // for (let char of stringA.toLowerCase().replace(/[^\w]/g, '')) {
+  //   charMapA[char] = charMapA[char] + 1 || 1;
+  // }
+
+  // for (let char of stringB.toLowerCase().replace(/[^\w]/g, '')) {
+  //   charMapB[char] = charMapB[char] + 1 || 1;
+  // }
+
+  // if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+  //   return false;
+  // }
+
+  // for (char in charMapB) {
+  //   if (charMapA[char] !== charMapB[char]) {
+  //     return false;
+  //   }
+  // }
+
+
+  // Alternate Solution
+  const cleanString = (str) => {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
+  };
+
+  let cleanedA = cleanString(stringA);
+  let cleanedB = cleanString(stringB);
+
+  
+  return cleanedA === cleanedB;
+}
 
 module.exports = anagrams;
