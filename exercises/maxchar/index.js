9bb53c47fe2777254099ce181c716f801e3c3fab
kalunge <muthomititus+github@gmail.com>
2022-01-25 11:10:28 +0100

    Add maximum character solution

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let charMap = {};
+  let max = 0;
+  let maxChar = '';
+
+  for (let char of str) {
+    if (!charMap[char]) {
+      charMap[char] = 1;
+    } else {
+      charMap[char]++;
+    }
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar
+}
 
 module.exports = maxChar;
+
+console.log(maxChar('apple 1231111'));
+console.log(maxChar('abcccccccd'));
