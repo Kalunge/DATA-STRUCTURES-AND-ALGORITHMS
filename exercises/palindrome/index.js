90bf88417a202f1466e36e29e7fe9238e5efba5b
kalunge <muthomititus+github@gmail.com>
2022-01-25 09:59:48 +0100

    Add palindrome solution

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,18 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// function palindrome(str) {
+//   const reversed = str.split("").reverse().join("")
+//   return str === reversed
+// }
+
+
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
 
 module.exports = palindrome;
+
+console.log(palindrome('abba'));
+console.log(palindrome('abcdefg'));
