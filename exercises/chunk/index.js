e05f528878e23b835814a2c6ad5e6817352166c7
kalunge <muthomititus+github@gmail.com>
2022-01-25 11:39:25 +0100

    Add Arraychunk solution

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  let result = [];
+
+  for (let item of array) {
+    if (
+      !result[result.length - 1] ||
+      result[result.length - 1].length === size
+    ) {
+      result.push([item]);
+    } else {
+      result[result.length - 1].push(item);
+    }
+  }
+  return result
+}
+
+console.log(chunk([1, 2, 3, 4, 5], 2));
 
 module.exports = chunk;
