4fd51ab4ae551712107ae696d6f5db8f0b0425e2
kalunge <muthomititus+github@gmail.com>
2022-02-17 18:22:44 +0100

    Add tree traversal solution

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,50 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
 
-class Tree {}
+  add(data) {
+    this.children.push(new Node(data));
+  }
+
+  remove(data) {
+    this.children = this.children.filter((node) => node.data !== data);
+  }
+}
+
+class Tree {
+  constructor() {
+    this.root = null;
+  }
+
+  traverseBF(fn) {
+    let result = [this.root];
+
+    while (result.length) {
+      const node = result.shift();
+
+      result.push(...node.children);
+
+      fn(node);
+    }
+    return result;
+  }
+
+  traverseDF(fn) {
+    let result = [this.root];
+
+    while (result.length) {
+      const node = result.shift();
+
+      result.unshift(...node.children);
+
+      fn(node);
+    }
+  }
+}
 
 module.exports = { Tree, Node };
